a95dc1af750144592c566f3e080249139fe8c928
Swaggerlicious51 <canosethan1b51@gmail.com>
2026-09-26 19:39:21 +0800

    Add events data for various teams

---
+++ docs/src/js/data/events.js
@@ -0,0 +1,6 @@
+const EVENTS = [
+  { team:"CCS Wizards",     title:"Wizard's Most Wanted",             img:"pictures/wizardsEvent.png",  desc:"CCS organization GDGC's Chief Technology Officer claims that the moon landing was fake." },
+  { team:"NSG Pythons",     title:"Pythons VS Warriors",              img:"pictures/pythonsEvent.png",  desc:"NSG started the kick off and are completely on par with the undefeated ENG'G Warriors." },
+  { team:"ENG'G Warriors",  title:"Warriors Building an Iron Wall",   img:"pictures/warriorsEvent.png", desc:"ENG'G's volleyball iron wall has been impenetrable so far! How will they do against the fierce Wolves.." },
+  { team:"SBM Eagles",      title:"Eagles at the Top",                img:"pictures/eaglesEvent.png",   desc:"SBM reigns victorious as the Champions of TXC 2025!" },
+];
